c5fcb3aa26208ca7ee1b6395c1211649f4e84de1
Antonio Cicero <aixoniarutad@gmail.com>
2021-11-30 15:16:59 -0500

    feat: no dynamic styles because tailwind purge

--- src/components/players/PlayerStatsChart.vue
+++ src/components/players/PlayerStatsChart.vue
@@ -3,7 +3,7 @@
     <SectionHeading title="Stats" />
 
     <RadioGroup v-model="selected" class="mb-4 flex justify-center">
-      <div :class="`grid grid-cols-${stats.length} gap-3`">
+      <div :class="`grid grid-cols-3 gap-3`">
         <RadioGroupOption as="template" v-for="stat in stats" :key="stat.group.displayName" :value="stat.group.displayName" v-slot="{ active, checked }">
           <div :class="[active ? 'ring-2 ring-offset-2 ring-indigo-500' : '', checked ? 'bg-indigo-600 border-transparent text-white hover:bg-indigo-700' : 'bg-white border-gray-200 text-gray-900 hover:bg-gray-50', 'cursor-pointer border rounded-md py-3 px-3 flex items-center justify-center text-sm font-medium uppercase sm:flex-1']">
             <RadioGroupLabel as="p">
